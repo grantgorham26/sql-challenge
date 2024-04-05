756abb719cae38ccc62655c963a6fb97759cc2bf
Grant Gorham <grantegorham26@gmail.com>
2024-04-05 15:24:04 -0600

    Create date_engineering.sql

---
+++ date_engineering.sql
@@ -0,0 +1,42 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(15) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR (15) NOT NULL,
+	birth_date VARCHAR (15) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL,
+	sex VARCHAR(10) NOT NULL,
+	hire_date VARCHAR(20) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
